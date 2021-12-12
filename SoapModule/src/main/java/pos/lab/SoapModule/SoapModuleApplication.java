9154da78d4dfc169f642be41b96c70f384d6031d
AlexandruMelinte <62885005+AlexandruMelinte@users.noreply.github.com>
2021-12-12 20:50:01 +0200

    SOAP initailzr project

---
+++ SoapModule/src/main/java/pos/lab/SoapModule/SoapModuleApplication.java
@@ -0,0 +1,13 @@
+package pos.lab.SoapModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapModuleApplication.class, args);
+	}
+
+}
---
+++ SoapModule/src/test/java/pos/lab/SoapModule/SoapModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package pos.lab.SoapModule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
